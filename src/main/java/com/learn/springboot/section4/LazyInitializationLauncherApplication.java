abecdef34c285303de0fca72df26512473267a73
Esther Carrelle <esthcarelle@gmail.com>
2024-03-20 16:32:30 +0200

    Create another launcher application

--- src/main/java/com/learn/springboot/section4/LazyInitializationLauncherApplication.java
+++ src/main/java/com/learn/springboot/section4/LazyInitializationLauncherApplication.java
@@ -3,15 +3,37 @@ package com.learn.springboot.section4;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
+@Component
+class ClassA{
+
+}
+@Component
+@Lazy
+class ClassB{
+    private ClassA classA;
+
+    public ClassB(ClassA classA){
+        System.out.println("Initialization logic");
+        this.classA = classA;
+    }
+
+    public void doSomething() {
+        System.out.println("Dooooo");
+    }
+}
 @Configuration
 @ComponentScan
 public class LazyInitializationLauncherApplication {
     public static void main(String[] args) {
         try (var context = new AnnotationConfigApplicationContext(LazyInitializationLauncherApplication.class)) {
-            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+            System.out.println("Initialization of context completed");
+            context.getBean(ClassB.class).doSomething();
+
         }
     }
 }
