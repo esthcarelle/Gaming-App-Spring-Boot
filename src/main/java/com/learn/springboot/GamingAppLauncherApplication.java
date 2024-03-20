a00b0ed191bdc74ed41a987cb1dbe53c1fc02e36
Esther Carrelle <esthcarelle@gmail.com>
2024-03-20 11:37:38 +0200

    Renamed the main application

---
+++ src/main/java/com/learn/springboot/GamingAppLauncherApplication.java
@@ -0,0 +1,22 @@
+package com.learn.springboot;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("package com.learn.springboot")
+public class GamingAppLauncherApplication {
+
+    public static void main(String[] args) {
+
+        try (var context = new AnnotationConfigApplicationContext(GamingAppLauncherApplication.class)) {
+
+            context.getBean(Game.class).up();
+            context.getBean(GameRunner.class).run();
+        }
+
+    }
+
+
+}
