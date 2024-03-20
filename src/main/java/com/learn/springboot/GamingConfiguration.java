5b68323a62024e08207ff38aa255adcd616df6f2
Esther Carrelle <esthcarelle@gmail.com>
2024-03-20 11:38:02 +0200

    Remove the beans and let spring create it for us

--- src/main/java/com/learn/springboot/GamingConfiguration.java
+++ src/main/java/com/learn/springboot/GamingConfiguration.java
@@ -6,12 +6,5 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GamingConfiguration {
 
-    @Bean
-    public Game game(){
-        return new MarioGame();
-    }
-    @Bean
-    public GameRunner gameRunner(Game game){
-        return new GameRunner(game);
-    }
+
 }
