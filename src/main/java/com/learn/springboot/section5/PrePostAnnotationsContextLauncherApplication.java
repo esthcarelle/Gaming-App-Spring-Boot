88ceb6c3b355728f55f590a3d2983edf40809548
Esther Carrelle <esthcarelle@gmail.com>
2024-03-22 14:48:36 +0200

    Add postConstruct annotation

--- src/main/java/com/learn/springboot/section5/PrePostAnnotationsContextLauncherApplication.java
+++ src/main/java/com/learn/springboot/section5/PrePostAnnotationsContextLauncherApplication.java
@@ -1,5 +1,6 @@
 package com.learn.springboot.section5;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -7,7 +8,26 @@ import org.springframework.stereotype.Component;
 
 @Component
 class SomeClass{
+    private SomeDependency someDependency;
 
+    public SomeClass(SomeDependency someDependency){
+        super();
+        this.someDependency = someDependency;
+        System.out.println(someDependency);
+    }
+
+    @PostConstruct
+    public void initialize(){
+        someDependency.getReady();
+    }
+}
+
+@Component
+class SomeDependency{
+
+    public void getReady() {
+        System.out.println("Get Ready");
+    }
 }
 @Configuration
 @ComponentScan
