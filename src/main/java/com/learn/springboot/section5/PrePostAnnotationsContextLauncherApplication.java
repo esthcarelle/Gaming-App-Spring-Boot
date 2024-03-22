b64347e53a8ed0329d253316848bb2e000291285
Esther Carrelle <esthcarelle@gmail.com>
2024-03-22 14:45:29 +0200

    Create a component class

--- src/main/java/com/learn/springboot/section5/PrePostAnnotationsContextLauncherApplication.java
+++ src/main/java/com/learn/springboot/section5/PrePostAnnotationsContextLauncherApplication.java
@@ -3,8 +3,12 @@ package com.learn.springboot.section5;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
+@Component
+class SomeClass{
 
+}
 @Configuration
 @ComponentScan
 public class PrePostAnnotationsContextLauncherApplication {
